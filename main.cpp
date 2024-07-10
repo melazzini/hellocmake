8c066cc5ab3f8990735f988eb3f21a3b49e41adb
francisco melazzini <francisco.melazzini@physics.msu.ru>
2024-07-10 21:24:07 +0300

    added gtk

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
-
-int main() {
-  std::cout << "Hello World!" << std::endl;
-
-  return 0;
-}
+#include <gtk/gtk.h> 
+ 
+int main(int argc, char *argv[]) 
+{ 
+  GtkWidget *window; 
+  gtk_init (&argc, &argv); 
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL); 
+  gtk_widget_show (window); 
+  gtk_main (); 
+  return 0; 
+} 
